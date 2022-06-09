0018f868110afe6ec1d83f17b9ce7f09325cc6de
Mamadou Sylla <m27sylla@gmail.com>
2022-06-09 18:47:33 -0400

    responsive done

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const ratingsElement = document.querySelectorAll(".rate-element");
 const submitButton = document.querySelector(".submit");
+const rateChoice = document.getElementById("rate");
+const hiddenDiv = document.querySelector(".hidden");
 
 let rateNumber;
 
 for (let i = 0; i < ratingsElement.length; i++) {
-  ratingsElement[i].addEventListener("click", function () {
-    rateNumber = ratingsElement[i].textContent;
+	ratingsElement[i].addEventListener("click", function () {
+		rateNumber = ratingsElement[i].textContent;
 
-    resetActive();
-    ratingsElement[i].classList.add("active");
-  });
+		resetActive();
+		ratingsElement[i].classList.add("active");
+	});
 }
 
 function resetActive() {
-  for (let i = 0; i < ratingsElement.length; i++) {
-    ratingsElement[i].classList.remove("active");
-  }
+	for (let i = 0; i < ratingsElement.length; i++) {
+		ratingsElement[i].classList.remove("active");
+	}
 }
-
-
 
 /* sélectionner le boutton submit
 quand on click sur le bouton submit => afficher la nouvelle div hidden
 
 sélectionner le span id => si la note est supérieur à 0 remplacer le span sélectionné*/
 
+
+submitButton.addEventListener("click", () => {
+	rateChoice.textContent = rateNumber;
+	if (rate >= 1) {
+		console.log(rateNumber);
+	}
+	hiddenDiv.classList.toggle("hidden");
+});
